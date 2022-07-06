e293dac6ac0d7e0a68eb1c7e47ed42004680771b
Janusz Grabowski <jan@wit.edu.pl>
2022-07-06 22:07:36 +0200

    Expose AuthenticationManager

--- certsentry-admin-backend/src/main/java/pl/grabojan/certsentry/admin/security/SecurityConfig.java
+++ certsentry-admin-backend/src/main/java/pl/grabojan/certsentry/admin/security/SecurityConfig.java
@@ -7,6 +7,8 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.env.Environment;
 import org.springframework.http.HttpStatus;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -33,6 +35,10 @@ public class SecurityConfig  {
 	@Autowired
 	private Environment env;
 	
+	@Bean
+	public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+	return authenticationConfiguration.getAuthenticationManager();
+	}
 	
 	@Bean
     public UserDetailsService users() {
